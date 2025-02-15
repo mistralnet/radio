447056933572834095f15eb3830a2d016f1b13c6
mistralnet <yanir.dvir@gmail.com>
2025-02-15 16:20:00 +0200

    Update receiver.js

--- receiver.js
+++ receiver.js
@@ -1,1 +1,25 @@
+const context = cast.framework.CastReceiverContext.getInstance();
+const playerManager = context.getPlayerManager();
 
+playerManager.setMessageInterceptor(
+    cast.framework.messages.MessageType.LOAD,
+    request => {
+        console.log('LOAD request', request);
+
+        // Customize media information
+        const media = request.media;
+        if (media && media.metadata) {
+            media.metadata.title = "גלי צה"ל";
+            media.metadata.artist = "רדיו אינטרנט";
+        }
+
+        return request;
+    });
+
+// Add event listeners for playerManager
+playerManager.addEventListener(cast.framework.PlayerManagerEventType.STATUS, (event) => {
+  console.log('Player status event: ' + JSON.stringify(event.detailedReasonCode));
+});
+
+context.start();
+console.log("אפליקציית רדיו - מקלט מוכנה");
